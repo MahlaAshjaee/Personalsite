787dc2a725e5bdc61b02e091286142337f65bac4
mahla <ashjaee74@gmail.com>
2024-12-09 19:45:00 +0330

    fix git urls

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -6,22 +6,22 @@ import {
 
 export const projects = [
   {
-    title: "پروژه افزودن مقاله",
+    title: "پروژه افزودن مخاطبین",
     img: ContactManagement,
-    link: "https://github.com/MahlaAshjaee/javascriptProge",
+    link: "https://github.com/MahlaAshjaee/contact-manager-app",
+    info: "این یک پروژه ی ری اکت هست که کاربر میتونه مخاطب موجود رو ببینه اضافه ,حذف ,ویرایش و سرچ کنه ",
+  },
+  {
+    title: "پروژه افزودن مقاله",
+    img: MakingAnArticle,
+    link: "https://github.com/MahlaAshjaee/javascriptPrj",
     info: "  این یک پروژه ی جاوااسکریپت هست که کاربر میتونه لاگین کنه و مقاله های موجود رو ببینه اضافه ,حذف ,ویرایش و لایک کنه و همچنین سرچ انجام بده",
   },
 
   {
     title: "پروژه سایت شخصی",
-    img: MakingAnArticle,
-    link: "https://github.com/MahlaAshjaee/javascriptProge",
-    info: "این یک پروژه ی ری اکت هست که سایت شخصی هست و اطلاعات و پروژه هایی ک زدم به همراه لینک گیت هاپ و ایدی تلگرام و اینستا و واتساپ هم داخلش قرار دادم",
-  },
-  {
-    title: "پروژه افزودن مخاطبین",
     img: PersonalWebsite,
-    link: "https://github.com/MahlaAshjaee/javascriptProge",
-    info: "این یک پروژه ی ری اکت هست که کاربر میتونه مخاطب موجود رو ببینه اضافه ,حذف ,ویرایش و سرچ کنه ",
+    link: "https://github.com/MahlaAshjaee/Personalsite",
+    info: "این یک پروژه ی ری اکت هست که سایت شخصی هست و اطلاعات و پروژه هایی ک زدم به همراه لینک گیت هاپ و ایدی تلگرام و اینستا و واتساپ هم داخلش قرار دادم",
   },
 ];
